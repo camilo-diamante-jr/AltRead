a35bec4ebbea06e90c3413bd2ac29b77062fb478
camilo-diamante-jr <camilodiamantejr@gmail.com>
2025-05-20 14:44:11 +0800

    preloader.php had been move to views

--- app/preloader.php
+++
@@ -1,7 +0,0 @@
-  <section id="preloader">
-      <div class="loader-wrapper">
-          <div class="loader">
-              <div class="loader loader-inner"></div>
-          </div>
-      </div>
-  </section>
